4c968531d8819bb05dc1ea5eb2900fa5b094b83f
CarlEvans1 <carltonokoth94@gmail.com>
2024-09-17 08:46:18 +0300

    added validators and other models

--- Urbanhive/Properties/admin.py
+++ Urbanhive/Properties/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from Properties.models import Building
+from Properties.models import Building, Amenity, Tenant, TotalTenants, Caretaker
+
 # Register your models here.
 
 admin.site.register(Building)
+admin.site.register(Amenity)
+admin.site.register(Tenant)
+admin.site.register(TotalTenants)
+admin.site.register(Caretaker)
+
